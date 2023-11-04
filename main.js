294972c0e97b568905dfb0b8067e33e35fa83edb
Emre <118940184+Emre033@users.noreply.github.com>
2023-11-04 19:26:18 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+ BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
+
+const { AoiClient, LoadCommands } = require("aoi.js"); // Elleme
+
+const bot = new AoiClient({
+    token: "", // " İçine Bot Tokenini Gir
+    prefix: "", // " İçine Prefixini Gir
+    intents: ["Guilds", "GuildMessages", "MessageContent"], // İntentler
+    events: ["onMessage", "onInteractionCreate"], // Eventler
+    database: {
+        type: "aoi.db",
+        db: require("aoi.db"),
+        tables: ["main"],
+        path: "./database/",
+        extraOptions: {
+            dbType: "KeyValue"
+        }
+    }
+});
+
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./komutlar/") // Komutları Yükler
+
+
+// Değiskenler
+bot.variables ({
+  ornek:"ornek"
+})
